e916d232d30e1381bb221f68b6cdfcf62f5d0472
rbisb <rbisb@DESKTOP-MM26GKA>
2020-11-26 20:15:11 +0300

    Добавьте файлы проекта.

---
+++ Shkadun_Homeworks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Shkadun_Homeworks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
