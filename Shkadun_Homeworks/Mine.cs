011e9472460858c75c078f9568e820b6b39f65ca
rbisb <rbisb@DESKTOP-MM26GKA>
2020-11-26 20:25:39 +0300

    Load Code

---
+++ Shkadun_Homeworks/Mine.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Shkadun_Princess
+{
+    class Mine
+    {
+        public string Status { get; private set; }
+        public int Damage { get; private set; }
+
+        public void InactiveMine()
+        {
+            Status = "Inactive";
+        }
+
+        public Mine(int damage)
+        {
+            Status = "Active";
+            Damage = damage;
+        }
+    }
+}
--- Shkadun_Homeworks/Program.cs
+++ Shkadun_Homeworks/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Threading;
 
-namespace Shkadun_Homeworks
+namespace Shkadun_Princess
 {
     class Program
     {
         static void Main(string[] args)
-        { 
-            Console.WriteLine("Hello World!");
+        {
+            ConsoleWriteAndRead cwar = new ConsoleWriteAndRead();
+            Player player = new Player();
+            Map map = new Map();
+
+            map.GenerationMines();
+            map.DrowMap(player);
+
+            while (true)
+            {
+                switch (Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.W:
+                    case ConsoleKey.UpArrow: player.PlayerRunUp(map); break;
+                    case ConsoleKey.S:
+                    case ConsoleKey.DownArrow: player.PlayerRunDown(map); break;
+                    case ConsoleKey.D:
+                    case ConsoleKey.RightArrow: player.PlayerRunRight(map); break;
+                    case ConsoleKey.A:
+                    case ConsoleKey.LeftArrow: player.PlayerRunLeft(map); break;
+                    default: break;
+                }
+                if (player.Hp < 0)
+                {
+                    if (cwar.GameOver(1) == 0) { break; }
+                    map.StartNewGame(player);
+                }
+                else if (player.Hp > 10)
+                {
+                    if (cwar.GameOver(0) == 0) { break; }
+                    map.StartNewGame(player);
+                }
+                Thread.Sleep(200);
+            }
         }
     }
 }
