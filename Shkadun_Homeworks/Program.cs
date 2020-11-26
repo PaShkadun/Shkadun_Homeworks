ecf4306f90e53b110a1254cd2004d65b1dbfdc26
rbisb <rbisb@DESKTOP-MM26GKA>
2020-11-26 20:17:16 +0300

    Create

--- Shkadun_Homeworks/Program.cs
+++ Shkadun_Homeworks/Program.cs
@@ -5,7 +5,7 @@ namespace Shkadun_Homeworks
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
             Console.WriteLine("Hello World!");
         }
     }
